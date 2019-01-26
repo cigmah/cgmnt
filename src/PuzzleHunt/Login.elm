04e3bca3dcaaf9167c1a7ff0a864ab40709fde2a
Jamin Wu <Jamin.Wu07@gmail.com>
2019-01-26 12:14:09 +1100

    Nicer error messages for login failure.

---
+++ src/PuzzleHunt/Login.elm
@@ -0,0 +1,205 @@
+module PuzzleHunt.Login exposing (decodeAuthToken, encodeLogin, encodeSendToken, handleLoginEvent, handleOnChangeLoginEmail, handleOnChangeLoginToken, handleOnLogin, handleOnSendToken, handleReceivedLogin, handleReceivedSendToken, handleToggleLoginModal)
+
+import Http
+import Json.Decode as Decode
+import Json.Encode as Encode
+import Shared.ApiBase exposing (apiBase)
+import Shared.Init exposing (emptyLogin, emptyRegister)
+import Shared.Types exposing (LoginEvent(..), LoginInformation, Model, Msg(..), Route(..))
+
+
+handleLoginEvent msg model =
+    case msg of
+        ToggleLoginModal ->
+            handleToggleLoginModal model
+
+        OnChangeLoginEmail email ->
+            handleOnChangeLoginEmail model email
+
+        OnChangeLoginToken token ->
+            handleOnChangeLoginToken model token
+
+        OnSendToken ->
+            handleOnSendToken model
+
+        OnLogin ->
+            handleOnLogin model
+
+        ReceivedSendToken result ->
+            handleReceivedSendToken model result
+
+        ReceivedLogin result ->
+            handleReceivedLogin model result
+
+
+handleToggleLoginModal model =
+    let
+        loginState =
+            case model.loginInformation of
+                Just loginInformation ->
+                    case loginInformation.isLoadingSendToken of
+                        False ->
+                            case loginInformation.isLoadingLogin of
+                                True ->
+                                    Just loginInformation
+
+                                False ->
+                                    Nothing
+
+                        True ->
+                            Just loginInformation
+
+                Nothing ->
+                    Just emptyLogin
+    in
+    ( { model | loginInformation = loginState }, Cmd.none )
+
+
+handleOnChangeLoginEmail model email =
+    case model.loginInformation of
+        Just info ->
+            ( { model | loginInformation = Just { info | email = email } }, Cmd.none )
+
+        Nothing ->
+            ( model, Cmd.none )
+
+
+handleOnChangeLoginToken model token =
+    case model.loginInformation of
+        Just info ->
+            ( { model | loginInformation = Just { info | token = token } }, Cmd.none )
+
+        Nothing ->
+            ( model, Cmd.none )
+
+
+handleOnSendToken model =
+    case model.loginInformation of
+        Just info ->
+            let
+                cmd =
+                    Http.post
+                        { url = apiBase ++ "auth/email/"
+                        , body = info |> encodeSendToken |> Http.jsonBody
+                        , expect = Http.expectString <| LoginMsg << ReceivedSendToken
+                        }
+            in
+            ( { model | loginInformation = Just { info | isLoadingSendToken = True } }, cmd )
+
+        Nothing ->
+            ( model, Cmd.none )
+
+
+handleOnLogin model =
+    case model.loginInformation of
+        Just info ->
+            let
+                cmd =
+                    Http.post
+                        { url = apiBase ++ "callback/auth/"
+                        , body = info |> encodeLogin |> Http.jsonBody
+                        , expect = Http.expectJson (LoginMsg << ReceivedLogin) decodeAuthToken
+                        }
+            in
+            ( { model | loginInformation = Just { info | isLoadingLogin = True } }, cmd )
+
+        Nothing ->
+            ( model, Cmd.none )
+
+
+handleReceivedSendToken model result =
+    case model.loginInformation of
+        Just info ->
+            case result of
+                Ok value ->
+                    ( { model | loginInformation = Just { info | isLoadingSendToken = False, sendTokenResponse = Just value, message = Nothing } }, Cmd.none )
+
+                Err error ->
+                    case error of
+                        Http.Timeout ->
+                            ( { model | loginInformation = Just { info | isLoadingSendToken = False, sendTokenResponse = Nothing, message = Just "Sorry, the request timed out." } }, Cmd.none )
+
+                        Http.NetworkError ->
+                            ( { model | loginInformation = Just { info | isLoadingSendToken = False, sendTokenResponse = Nothing, message = Just "There was a network error." } }, Cmd.none )
+
+                        Http.BadStatus code ->
+                            case code of
+                                400 ->
+                                    ( { model | loginInformation = Just { info | isLoadingSendToken = False, sendTokenResponse = Nothing, message = Just "Hmm, we don't have that email in our registration list. Have you registered? Or maybe a typo?" } }, Cmd.none )
+
+                                _ ->
+                                    ( { model | loginInformation = Just { info | isLoadingSendToken = False, sendTokenResponse = Nothing, message = Just <| "There was an error with code " ++ String.fromInt code } }, Cmd.none )
+
+                        _ ->
+                            ( { model | loginInformation = Just { info | isLoadingSendToken = False, sendTokenResponse = Nothing, message = Just "There was an error." } }, Cmd.none )
+
+        Nothing ->
+            ( model, Cmd.none )
+
+
+handleReceivedLogin model result =
+    case model.loginInformation of
+        Just info ->
+            case result of
+                Ok value ->
+                    ( { model
+                        | authToken = Just value
+                        , loginInformation = Just { info | isLoadingLogin = False, message = Nothing }
+                      }
+                    , Cmd.none
+                    )
+
+                Err error ->
+                    case error of
+                        Http.Timeout ->
+                            ( { model
+                                | authToken = Nothing
+                                , loginInformation = Just { info | isLoadingLogin = False, message = Just "Sorry, the request timed out" }
+                              }
+                            , Cmd.none
+                            )
+
+                        Http.BadStatus code ->
+                            let
+                                message =
+                                    case code of
+                                        400 ->
+                                            "That token doesn't seem to be correct. Maybe a typo?"
+
+                                        _ ->
+                                            "There was an error with code " ++ String.fromInt code
+                            in
+                            ( { model
+                                | authToken = Nothing
+                                , loginInformation = Just { info | isLoadingLogin = False, message = Just message }
+                              }
+                            , Cmd.none
+                            )
+
+                        _ ->
+                            ( { model
+                                | authToken = Nothing
+                                , loginInformation = Just { info | isLoadingLogin = False, message = Just "There was an error." }
+                              }
+                            , Cmd.none
+                            )
+
+        Nothing ->
+            ( model, Cmd.none )
+
+
+encodeSendToken : LoginInformation -> Encode.Value
+encodeSendToken info =
+    Encode.object
+        [ ( "email", Encode.string info.email ) ]
+
+
+encodeLogin : LoginInformation -> Encode.Value
+encodeLogin info =
+    Encode.object
+        [ ( "token", Encode.string info.token ) ]
+
+
+decodeAuthToken : Decode.Decoder String
+decodeAuthToken =
+    Decode.field "token" Decode.string
---
+++ src/PuzzleHunt/Register.elm
@@ -0,0 +1,143 @@
+module PuzzleHunt.Register exposing (encodeRegister, handleOnChangeRegisterEmail, handleOnChangeRegisterFirstName, handleOnChangeRegisterLastName, handleOnChangeRegisterUsername, handleOnRegister, handleReceivedRegister, handleRegisterEvent, handleToggleRegisterModal)
+
+import Http
+import Json.Decode as Decode
+import Json.Encode as Encode
+import Shared.ApiBase exposing (apiBase)
+import Shared.Init exposing (emptyLogin, emptyRegister)
+import Shared.Types exposing (Model, Msg(..), RegisterEvent(..), RegisterInformation, Route(..))
+
+
+handleRegisterEvent msg model =
+    case msg of
+        ToggleRegisterModal ->
+            handleToggleRegisterModal model
+
+        OnChangeRegisterEmail email ->
+            handleOnChangeRegisterEmail model email
+
+        OnChangeRegisterUsername username ->
+            handleOnChangeRegisterUsername model username
+
+        OnChangeRegisterFirstName firstName ->
+            handleOnChangeRegisterFirstName model firstName
+
+        OnChangeRegisterLastName lastName ->
+            handleOnChangeRegisterLastName model lastName
+
+        OnRegister ->
+            handleOnRegister model
+
+        ReceivedRegister result ->
+            handleReceivedRegister model result
+
+
+handleToggleRegisterModal model =
+    let
+        registerState =
+            case model.registerInformation of
+                Just registerInformation ->
+                    case registerInformation.isLoading of
+                        True ->
+                            Just registerInformation
+
+                        False ->
+                            Nothing
+
+                Nothing ->
+                    Just emptyRegister
+    in
+    ( { model | registerInformation = registerState }, Cmd.none )
+
+
+handleOnChangeRegisterUsername model username =
+    case model.registerInformation of
+        Just info ->
+            ( { model | registerInformation = Just { info | username = username } }, Cmd.none )
+
+        Nothing ->
+            ( model, Cmd.none )
+
+
+handleOnChangeRegisterEmail model email =
+    case model.registerInformation of
+        Just info ->
+            ( { model | registerInformation = Just { info | email = email } }, Cmd.none )
+
+        Nothing ->
+            ( model, Cmd.none )
+
+
+handleOnChangeRegisterFirstName model firstName =
+    case model.registerInformation of
+        Just info ->
+            ( { model | registerInformation = Just { info | firstName = firstName } }, Cmd.none )
+
+        Nothing ->
+            ( model, Cmd.none )
+
+
+handleOnChangeRegisterLastName model lastName =
+    case model.registerInformation of
+        Just info ->
+            ( { model | registerInformation = Just { info | lastName = lastName } }, Cmd.none )
+
+        Nothing ->
+            ( model, Cmd.none )
+
+
+handleOnRegister model =
+    case model.registerInformation of
+        Just info ->
+            let
+                cmd =
+                    Http.post
+                        { url = apiBase ++ "users/"
+                        , body = info |> encodeRegister |> Http.jsonBody
+                        , expect = Http.expectString <| RegisterMsg << ReceivedRegister
+                        }
+            in
+            ( { model | registerInformation = Just { info | isLoading = True } }, cmd )
+
+        Nothing ->
+            ( model, Cmd.none )
+
+
+handleReceivedRegister model result =
+    case model.registerInformation of
+        Just info ->
+            case result of
+                Ok value ->
+                    ( { model | registerInformation = Just { info | isLoading = False, response = Just value, message = Just "Registration completed succesfully! Proceed to login." } }, Cmd.none )
+
+                Err error ->
+                    case error of
+                        Http.Timeout ->
+                            ( { model | registerInformation = Just { info | isLoading = False, response = Nothing, message = Just "Sorry, the request timed out!" } }, Cmd.none )
+
+                        Http.NetworkError ->
+                            ( { model | registerInformation = Just { info | isLoading = False, response = Nothing, message = Just "There was a network error." } }, Cmd.none )
+
+                        Http.BadStatus code ->
+                            case code of
+                                400 ->
+                                    ( { model | registerInformation = Just { info | isLoading = False, response = Nothing, message = Just <| "Either this username or email has already been registered, or something was wrong with your inputs. Make sure you at least have both a username and a valid email." } }, Cmd.none )
+
+                                _ ->
+                                    ( { model | registerInformation = Just { info | isLoading = False, response = Nothing, message = Just <| "There was an error with error code " ++ String.fromInt code ++ "." } }, Cmd.none )
+
+                        _ ->
+                            ( { model | registerInformation = Just { info | isLoading = False, response = Nothing, message = Just "There was an error." } }, Cmd.none )
+
+        Nothing ->
+            ( model, Cmd.none )
+
+
+encodeRegister : RegisterInformation -> Encode.Value
+encodeRegister info =
+    Encode.object
+        [ ( "username", Encode.string info.username )
+        , ( "email", Encode.string info.email )
+        , ( "first_name", Encode.string info.firstName )
+        , ( "last_name", Encode.string info.lastName )
+        ]
