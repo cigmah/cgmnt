f9c76794e7cb7722f37ac7e469149ddfba92731a
Jamin Wu <Jamin.Wu07@gmail.com>
2019-01-30 15:59:40 +1100

    Submissions page functional, no view.

--- src/Page/Submissions.elm
+++ src/Page/Submissions.elm
@@ -1,9 +1,20 @@
 module Page.Submissions exposing (Model, Msg, init, subscriptions, toSession, update, view)
 
-import Html exposing (Html, div, h1, text)
-import Html.Attributes exposing (class)
+import Api
+import Decoders exposing (decoderPuzzleSet, decoderThemeSet)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Html.Lazy exposing (..)
+import Iso8601
+import Json.Decode as Decode exposing (Decoder)
+import Json.Decode.Pipeline exposing (optional, required)
+import Json.Encode as Encode
 import RemoteData exposing (RemoteData(..), WebData)
-import Session exposing (Session)
+import Session exposing (Session(..))
+import Time exposing (Posix)
+import Types exposing (..)
+import Viewer exposing (Viewer(..))
 
 
 
@@ -12,14 +23,61 @@ import Session exposing (Session)
 
 type alias Model =
     { session : Session
+    , state : State
+    }
+
+
+type State
+    = Denied
+    | Accepted (WebData SubmissionsData)
+
+
+type alias SubmissionsData =
+    List UserSubmission
+
+
+type alias UserSubmission =
+    { username : String
+    , puzzleTitle : String
+    , theme : String
+    , set : PuzzleSet
+    , submissionDatetime : Posix
+    , submission : String
+    , isCorrect : Bool
+    , points : Int
     }
 
 
 init : Session -> ( Model, Cmd Msg )
 init session =
-    ( { session = session }
-    , Cmd.none
-    )
+    case session of
+        LoggedIn _ viewer ->
+            ( { session = session, state = Accepted Loading }
+            , Api.get "submissions/user/" (Just <| Viewer.cred viewer) ReceivedData decoderSubmissionsData
+            )
+
+        Guest _ ->
+            ( { session = session, state = Denied }
+            , Cmd.none
+            )
+
+
+decoderUserSubmission : Decoder UserSubmission
+decoderUserSubmission =
+    Decode.map8 UserSubmission
+        (Decode.field "user" <| Decode.field "username" Decode.string)
+        (Decode.field "puzzle" <| Decode.field "title" Decode.string)
+        (Decode.field "puzzle" <| Decode.field "theme" <| Decode.field "theme" Decode.string)
+        (Decode.field "puzzle" <| Decode.field "puzzle_set" decoderPuzzleSet)
+        (Decode.field "submission_datetime" Iso8601.decoder)
+        (Decode.field "submission" Decode.string)
+        (Decode.field "is_response_correct" Decode.bool)
+        (Decode.field "points" Decode.int)
+
+
+decoderSubmissionsData : Decoder SubmissionsData
+decoderSubmissionsData =
+    Decode.list decoderUserSubmission
 
 
 toSession : Model -> Session
@@ -33,6 +91,7 @@ toSession model =
 
 type Msg
     = GotSession Session
+    | ReceivedData (WebData SubmissionsData)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -40,6 +99,9 @@ update msg model =
     case msg of
         GotSession session ->
             ( { model | session = session }, Cmd.none )
+
+        ReceivedData response ->
+            ( { model | state = Accepted response }, Cmd.none )
 
 
 
@@ -67,7 +129,7 @@ mainHero =
         [ div [ class "hero-body" ]
             [ div [ class "container" ]
                 [ div [ class "columns is-multiline" ]
-                    [ div [] [ text "TEST" ]
+                    [ div [] [ text "Submissions" ]
                     ]
                 ]
             ]
