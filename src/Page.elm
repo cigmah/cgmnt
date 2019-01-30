b19fecac909484bff5bdf2a2901d6ae233e6b989
Jamin Wu <Jamin.Wu07@gmail.com>
2019-01-30 19:48:15 +1100

    Draft homepage.

--- src/Page.elm
+++ src/Page.elm
@@ -30,51 +30,6 @@ view maybeViewer page { title, content } =
     }
 
 
-viewHeader : Page -> Maybe Viewer -> Html msg
-viewHeader page maybeViewer =
-    nav [ class "navbar" ]
-        [ div [ class "navbar-brand" ]
-            [ a [ class "navbar-item", Route.href Route.Home ]
-                [ text "CIGMAH Puzzle Hunt 2019" ]
-            , a [ class "navbar-burger burger" ]
-                [ span [] [], span [] [], span [] [] ]
-            ]
-        , div [ class "navbar-menu" ]
-            [ div [ class "navbar-start" ] <|
-                navbarLink page Route.Home [ text "Home" ]
-                    :: navbarLink page Route.Resources [ text "Resources" ]
-                    :: navbarLink page Route.Archive [ text "Archive" ]
-                    :: []
-            , div [ class "navbar-end" ] <| viewMenu page maybeViewer
-            ]
-        ]
-
-
-viewMenu : Page -> Maybe Viewer -> List (Html msg)
-viewMenu page maybeViewer =
-    let
-        linkTo =
-            navbarLink page
-    in
-    case maybeViewer of
-        Just viewer ->
-            let
-                username =
-                    Viewer.username viewer
-            in
-            [ linkTo Route.Dashboard [ text "Dashboard" ]
-            , linkTo Route.OpenPuzzles [ text "Open Puzzles" ]
-            , linkTo Route.ClosedPuzzles [ text "Closed Puzzles" ]
-            , linkTo Route.Leaderboard [ text "Leaderboard" ]
-            , linkTo Route.Submissions [ text "Submissions" ]
-            , li [ class "navbar-item" ] [ p [] [ text username ] ]
-            , li [ class "navbar-item" ] [ a [ Route.href Route.Logout ] [ text "Logout" ] ]
-            ]
-
-        Nothing ->
-            [ linkTo Route.Login [ text "Login" ] ]
-
-
 
 --[ linkTo Route.Login [ text "Sign in" ]
 --, linkTo Route.Register [ text "Sign up" ]
@@ -87,31 +42,6 @@ viewFooter =
         [ div [ class "container" ]
             [ text "footer" ]
         ]
-
-
-navbarLink : Page -> Route -> List (Html msg) -> Html msg
-navbarLink page route linkContent =
-    li [ classList [ ( "navbar-item", True ), ( "active", isActive page route ) ] ]
-        [ a [ class "nav-link", Route.href route ] linkContent ]
-
-
-isActive : Page -> Route -> Bool
-isActive page route =
-    case ( page, route ) of
-        ( Home, Route.Home ) ->
-            True
-
-        ( Resources, Route.Resources ) ->
-            True
-
-        ( Archive, Route.Archive ) ->
-            True
-
-        ( Login, Route.Login ) ->
-            True
-
-        _ ->
-            False
 
 
 {-| Render dismissable errors. We use this all over the place!
--- src/Page/Home.elm
+++ src/Page/Home.elm
@@ -2,8 +2,9 @@ module Page.Home exposing (Model, Msg, init, subscriptions, toSession, update, v
 
 import Api
 import Decoders exposing (decoderLeaderTotal)
-import Html exposing (Html, div, h1, text)
-import Html.Attributes exposing (class)
+import Html exposing (Html, button, div, footer, h1, input, label, span, table, tbody, td, text, th, thead, tr)
+import Html.Attributes exposing (class, classList, placeholder, type_)
+import Html.Events exposing (onInput, onSubmit)
 import Html.Lazy exposing (..)
 import Json.Decode as Decode exposing (Decoder)
 import Json.Encode as Encode
@@ -74,6 +75,8 @@ toSession model =
 type Msg
     = ChangeRegisterUsername String
     | ChangeRegisterEmail String
+    | ChangeRegisterFirstName String
+    | ChangeRegisterLastName String
     | ClickedRegister
     | ReceivedRegisterResponse (WebData Response)
     | ReceivedMiniLeader (WebData LeaderTotalData)
@@ -89,6 +92,12 @@ update msg model =
 
         ChangeRegisterEmail input ->
             ( { model | email = input }, Cmd.none )
+
+        ChangeRegisterFirstName input ->
+            ( { model | firstName = input }, Cmd.none )
+
+        ChangeRegisterLastName input ->
+            ( { model | lastName = input }, Cmd.none )
 
         ClickedRegister ->
             case model.registerResponse of
@@ -133,28 +142,203 @@ navMenuLinked model body =
 view : Model -> { title : String, content : Html Msg }
 view model =
     { title = "CIGMAH"
-    , content = navMenuLinked model <| mainHero
+    , content = navMenuLinked model <| mainHero model
     }
 
 
-mainHero =
-    div [ class "hero is-primary is-fullheight-with-navbar" ]
-        [ div [ class "hero-body" ]
-            [ div [ class "container" ]
-                [ div [ class "columns is-multiline" ]
-                    [ tableColumn
-                    , registerColumn
+mainHero model =
+    let
+        isLoading =
+            case model.miniLeader of
+                Loading ->
+                    True
+
+                _ ->
+                    False
+    in
+    div []
+        [ div [ classList [ ( "pageloader", True ), ( "is-active", isLoading ) ] ] [ span [ class "title" ] [ text "Loading..." ] ]
+        , div [ class "hero is-primary is-fullheight-with-navbar" ]
+            [ div [ class "hero-body" ]
+                [ div [ class "container" ]
+                    [ div [ class "tile is-ancestor" ]
+                        [ div [ class "tile is-parent is-vertical" ]
+                            [ div [ class "tile is-parent is-12" ]
+                                [ div [ class "tile is-child" ]
+                                    [ div [ class "subtitle has-text-centered" ] [ text "Coding Interest Group in Medicine and Healthcare presents the " ]
+                                    , div [ class "title has-text-centered" ] [ text "CIGMAH Puzzle Hunt 2019" ]
+                                    ]
+                                ]
+                            , div [ class "tile is-parent is-12" ]
+                                [ div [ class "tile is-child columns is-multiline" ]
+                                    [ tableColumn model
+                                    , registerColumn model
+                                    ]
+                                ]
+                            ]
+                        ]
                     ]
                 ]
             ]
         ]
 
 
-tableColumn =
-    div [ class "column" ]
-        [ div [ class "title" ] [ text "Leaderboard" ] ]
+makeHeaderCell headerText =
+    th [] [ text headerText ]
 
 
-registerColumn =
-    div [ class "column" ]
-        [ div [ class "title" ] [ text "Registration" ] ]
+makeRow rank leaderUnit =
+    tr []
+        [ td [] [ text <| String.fromInt rank ]
+        , td [] [ text leaderUnit.username ]
+        , td [] [ text <| String.fromInt leaderUnit.total ]
+        ]
+
+
+tableColumn model =
+    let
+        tableHeaders =
+            [ "Rank", "Username", "Points" ]
+
+        tableHead =
+            tr [] <| List.map makeHeaderCell tableHeaders
+
+        ranks miniLeader =
+            List.range 1 (List.length miniLeader)
+
+        rows miniLeader =
+            List.map2 makeRow (ranks miniLeader) miniLeader
+    in
+    case model.miniLeader of
+        Success miniLeader ->
+            div [ class "column is-half" ]
+                [ div [ class "card" ]
+                    [ div [ class "card-header" ]
+                        [ div [ class "card-header-title" ] [ text "Leaderboard" ] ]
+                    , div [ class "card-content" ]
+                        [ table [ class "table is-fullwidth is-hoverable" ]
+                            [ thead [] [ tableHead ], tbody [] <| rows miniLeader ]
+                        ]
+                    ]
+                ]
+
+        _ ->
+            div [] []
+
+
+registerColumn model =
+    div [ class "column is-half" ] <|
+        registerForm model
+
+
+registerForm model =
+    let
+        colourState =
+            case model.registerResponse of
+                Success _ ->
+                    "is-success"
+
+                _ ->
+                    ""
+
+        loadingState =
+            case model.registerResponse of
+                Loading ->
+                    " is-loading "
+
+                _ ->
+                    ""
+
+        registerButton =
+            case model.registerResponse of
+                Success _ ->
+                    div [] []
+
+                _ ->
+                    div [ class "field is-horizontal" ]
+                        [ div [ class "field-body" ]
+                            [ div [ class "field " ]
+                                [ div [ class "control" ]
+                                    [ button
+                                        [ type_ "submit"
+                                        , class <| "button is-medium is-fullwidth" ++ loadingState ++ colourState
+                                        ]
+                                        [ text "Register" ]
+                                    ]
+                                ]
+                            ]
+                        ]
+
+        message =
+            case model.registerResponse of
+                NotAsked ->
+                    div [] []
+
+                Loading ->
+                    div [] []
+
+                Success msg ->
+                    footer [ class "card-footer has-text-white is-success has-background-success" ] [ span [ class "card-footer-item content has-text-centered" ] [ text msg ] ]
+
+                Failure error ->
+                    footer [ class "card-footer has-text-white has-background-danger" ] [ span [ class "card-footer-item content has-text-centered" ] [ text "There was an error." ] ]
+    in
+    [ div [ class "card" ]
+        [ div [ class "card-header" ]
+            [ span [ class <| "card-header-title has-text-weight-semibold is-centered has-text-centered" ++ colourState ]
+                [ text "Registrations open." ]
+            ]
+        , Html.form
+            [ class "card-content "
+            , onSubmit ClickedRegister
+            ]
+            [ registerField "Username" "bms_intern" "text" <| ChangeRegisterUsername
+            , registerField "Email" "ray.basch@bestmedicalschool.com" "email" <| ChangeRegisterEmail
+            , div [ class "field is-horizontal" ]
+                [ div [ class "field-label is-normal " ] [ label [ class "label " ] [ text "Name" ] ]
+                , div [ class "field-body" ]
+                    [ div [ class "field " ]
+                        [ div [ class "control" ]
+                            [ input
+                                [ class "input "
+                                , type_ "text"
+                                , placeholder "Ray"
+                                , onInput <| ChangeRegisterFirstName
+                                ]
+                                []
+                            ]
+                        ]
+                    , div [ class "field" ]
+                        [ div [ class "control" ]
+                            [ input
+                                [ class "input "
+                                , type_ "text"
+                                , placeholder "Basch"
+                                , onInput <| ChangeRegisterLastName
+                                ]
+                                []
+                            ]
+                        ]
+                    ]
+                ]
+            , registerButton
+            ]
+        , message
+        ]
+    ]
+
+
+registerField : String -> String -> String -> (String -> Msg) -> Html Msg
+registerField fieldLabel fieldPlaceholder fieldType fieldOnChange =
+    div [ class "field is-horizontal" ]
+        [ div [ class "field-label is-normal" ]
+            [ label [ class "label " ] [ text fieldLabel ] ]
+        , div [ class "field-body  is-expanded" ]
+            [ div [ class "field" ]
+                [ div [ class "control is-expanded" ]
+                    [ input [ class "input ", type_ fieldType, placeholder fieldPlaceholder, onInput fieldOnChange ]
+                        []
+                    ]
+                ]
+            ]
+        ]
