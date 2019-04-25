c965c956d326afd64097ce16d5626cc176e45501
Jamin Wu <Jamin.Wu07@gmail.com>
2019-04-26 05:48:13 +1000

    Register page done.

--- src/Views/Register.elm
+++ src/Views/Register.elm
@@ -34,44 +34,32 @@ view meta registerState =
 registerPage : RegisterState -> Html Msg
 registerPage state =
     let
-        messageDiv =
+        messageText =
             case state of
                 NewUser _ (Success _) ->
-                    div
-                        [ class "bg-grey-lighter border-red text-grey-darkest border-l-2 rounded rounded-l-none p-4 mt-3" ]
-                        [ text "Registration was successful! Head over to the ", a [ routeHref LoginRoute ] [ text "login tab" ], text " to start logging in!" ]
+                    [ text "Registration was successful! Head over to the ", a [ routeHref LoginRoute ] [ text "login tab" ], text " to start logging in!" ]
 
                 NewUser _ (Failure e) ->
                     case e of
                         BadStatus errorData ->
                             case errorData.status.code of
                                 400 ->
-                                    div
-                                        [ class "bg-grey-lighter border-red text-grey-darkest border-l-2 rounded rounded-l-none p-4 mt-3" ]
-                                        [ text "There was a problem with that username or email - either they were invalid (make sure there are no spaces!) or they're taken. Either way, you'll have to change one or both of them (sorry!)" ]
+                                    [ text "There was a problem with that username or email - either they were invalid (make sure there are no spaces!) or they're taken. Either way, you'll have to change one or both of them (sorry!)" ]
 
                                 code ->
-                                    div
-                                        [ class "bg-grey-lighter border-red text-grey-darkest border-l-2 rounded rounded-l-none p-4 mt-3" ]
-                                        [ text <| "It seems like there was an error...sorry about that. Let us know! To help us, let us know that the status code was " ++ String.fromInt code ]
+                                    [ text <| "It seems like there was an error...sorry about that. Let us know! To help us, let us know that the status code was " ++ String.fromInt code ]
 
                         NetworkError ->
-                            div
-                                [ class "bg-grey-lighter border-red text-grey-darkest border-l-2 rounded rounded-l-none p-4 mt-3" ]
-                                [ text "There was something wrong with the network. Is your internet working?" ]
+                            [ text "There was something wrong with the network. Is your internet working?" ]
 
                         _ ->
-                            div
-                                [ class "bg-grey-lighter border-red text-grey-darkest border-l-2 rounded rounded-l-none p-4 mt-3" ]
-                                [ text "It seems like there was an error...sorry about that. Let us know!" ]
+                            [ text "It seems like there was an error...sorry about that. Let us know!" ]
 
                 AlreadyUser ->
-                    div
-                        [ class "bg-grey-lighter border-red text-grey-darkest border-l-2 rounded rounded-l-none p-4 mt-3" ]
-                        [ text "You've already registered...in fact, you're logged in right now!" ]
+                    [ text "You've already registered...in fact, you're logged in right now!" ]
 
                 _ ->
-                    div [] []
+                    []
 
         hideForm =
             case state of
@@ -84,132 +72,38 @@ registerPage state =
                 _ ->
                     False
 
-        usernameColour =
-            case state of
-                NewUser data _ ->
-                    case data.username of
-                        "" ->
-                            "red"
-
-                        _ ->
-                            case String.contains " " data.username of
-                                True ->
-                                    "red"
-
-                                False ->
-                                    "green"
-
-                _ ->
-                    "grey"
-
-        emailColour =
-            case state of
-                NewUser data _ ->
-                    case String.contains "@" data.email && String.contains "." data.email of
-                        True ->
-                            "green"
-
-                        False ->
-                            "red"
-
-                _ ->
-                    "grey"
-
         buttonText =
             case state of
                 NewUser _ Loading ->
                     "Loading..."
 
                 _ ->
-                    "Submit"
+                    "Register"
     in
-    div
-        [ class "px-2 md:px-8 bg-grey-lightest" ]
-        [ div
-            [ class "flex flex-wrap md:h-screen content-center justify-center items-center pt-16 pb-12 md:pb-0 md:pt-12 md:pt-12" ]
-            [ Html.form
-                [ class "block md:w-3/4 lg:w-2/3 xl:w-1/2", onSubmit RegisterClicked ]
-                [ div
-                    [ class "inline-flex flex justify-center w-full" ]
-                    [ div
-                        [ class "flex items-center  sm:text-xl justify-center sm:h-12 sm:w-10 px-5 py-3 rounded-l-lg font-black bg-red-light text-grey-lighter border-b-2 border-red-dark" ]
-                        [ span
-                            [ class "fas fa-user-plus" ]
-                            []
-                        ]
+    div [ class "main" ]
+        [ div [ class "container" ]
+            [ div [ class "center" ]
+                [ ul [ class "register-list" ]
+                    [ li [] [ text "We email you login tokens to verify your identity." ]
+                    , li [] [ text "We don't spam you or share your email." ]
+                    , li [] [ text "Don't use your email as your username." ]
+                    , li [] [ text "After registering, go to the login tab." ]
+                    ]
+                , Html.form [ class "register", onSubmit RegisterClicked, classList [ ( "hide", hideForm ) ] ]
+                    [ div [ class "form-control" ]
+                        [ input [ type_ "text", placeholder "Username*", onInput RegisterChangedUsername ] [] ]
                     , div
-                        [ class "flex items-center w-full p-3 px-5 sm:h-12 rounded-r-lg text-grey-darkest sm:text-lg font-bold uppercase bg-grey-lighter border-b-2 border-grey" ]
-                        [ text "Register for the 2019 Puzzle Hunt" ]
-                    ]
-                , div
-                    [ class "block w-full my-3 bg-white rounded-lg p-6 w-full text-base border-b-2 border-grey-light" ]
-                    [ p
-                        []
-                        [ text "We use a password-less login system similar to"
-                        , a
-                            [ class "text-blue no-underline", href "https://blog.medium.com/signing-in-to-medium-by-email-aacc21134fcd" ]
-                            [ text " one coined by Medium." ]
-                        , br
-                            []
-                            []
-                        , br
-                            []
-                            []
-                        , text
-                            "All we require is a username and email - a name is optional."
-                        ]
-                    , br
-                        []
-                        []
-                    , p
-                        []
-                        [ text "Once you've registered, you can start logging in immediately. When you login, a short-term code will be sent to your email to confirm your identity. We never share your email, and will only email you to send you login codes and liaise with you if you are awarded a prize." ]
+                        [ class "form-control" ]
+                        [ input [ type_ "email", placeholder "Email*", onInput RegisterChangedEmail ] [] ]
                     , div
-                        [ class "block w-full px-1 py-4 md:px-4 pb-1 mt-1", classList [ ( "hidden", hideForm ) ] ]
-                        [ input
-                            [ class <| "w-full my-1 mt-0 px-4 py-2 rounded-lg bg-grey-lighter outline-none text-grey-darker focus:bg-grey-lightest focus:text-grey-darkest border-l-2 rounded-l-none border-" ++ usernameColour ++ "-light focus:border-" ++ usernameColour
-                            , onInput RegisterChangedUsername
-                            , placeholder "Username"
-                            ]
-                            []
-                        , input
-                            [ class <| "w-full my-1 px-4 py-2 rounded-lg bg-grey-lighter outline-none text-grey-darker focus:bg-grey-lightest focus:text-grey-darkest border-l-2 rounded-l-none border-" ++ emailColour ++ "-light focus:border-" ++ emailColour
-                            , onInput RegisterChangedEmail
-                            , placeholder "Email"
-                            , type_ "email"
-                            ]
-                            []
-                        , div
-                            [ class "sm:flex inline-flex flex-wrap w-full" ]
-                            [ div
-                                [ class "w-full md:w-1/2 md:pr-1" ]
-                                [ input
-                                    [ class "w-full my-1 px-4 py-2 rounded-lg bg-grey-lighter outline-none text-grey-darker focus:bg-grey-lightest focus:text-grey-darkest"
-                                    , onInput RegisterChangedFirstName
-                                    , placeholder "First Name"
-                                    ]
-                                    []
-                                ]
-                            , div
-                                [ class "w-full md:w-1/2 md:pl-1" ]
-                                [ input
-                                    [ class "w-full my-1 px-4 py-2 rounded-lg bg-grey-lighter outline-none text-grey-darker focus:bg-grey-lightest focus:text-grey-darkest"
-                                    , onInput RegisterChangedLastName
-                                    , placeholder "Last Name"
-                                    ]
-                                    []
-                                ]
-                            ]
-                        ]
-                    , messageDiv
+                        [ class "form-control" ]
+                        [ input [ type_ "text", placeholder "First Name", onInput RegisterChangedFirstName ] [] ]
+                    , div
+                        [ class "form-control" ]
+                        [ input [ type_ "text", placeholder "Last Name", onInput RegisterChangedLastName ] [] ]
+                    , button [] [ text buttonText ]
                     ]
-                , div
-                    [ class "flex w-full justify-center", classList [ ( "hidden invisible", hideForm ) ] ]
-                    [ button
-                        [ class "px-3 py-2 bg-red rounded-lg border-b-2 border-red-dark w-full md:w-1/2 text-white active:border-0 outline-none focus:outline-none active:outline-none hover:border-b-4"
-                        ]
-                        [ text buttonText ]
-                    ]
+                , div [ class "register message" ] messageText
                 ]
             ]
         ]
