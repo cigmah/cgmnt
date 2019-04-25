12d431968add6ce22d05ef64689ac7df63bf84aa
Jamin Wu <Jamin.Wu07@gmail.com>
2019-04-26 00:18:55 +1000

    Puzzle list page.

--- src/Views/PuzzleList.elm
+++ src/Views/PuzzleList.elm
@@ -25,15 +25,7 @@ view meta puzzleListState =
                             loadingPage
 
                         Success puzzlePageData ->
-                            if List.length puzzlePageData.puzzles < 25 then
-                                let
-                                    numRepeats =
-                                        25 - List.length puzzlePageData.puzzles
-                                in
-                                puzzleListPage False Nothing { puzzlePageData | puzzles = puzzlePageData.puzzles ++ List.repeat numRepeats defaultPuzzleData } PuzzleListClickedPuzzle
-
-                            else
-                                puzzleListPage False Nothing puzzlePageData PuzzleListClickedPuzzle
+                            puzzleListPage puzzlePageData PuzzleListClickedPuzzle
 
                         Failure e ->
                             errorPage ""
@@ -47,15 +39,7 @@ view meta puzzleListState =
                             loadingPage
 
                         Success puzzlePageData ->
-                            if List.length puzzlePageData.puzzles < 25 then
-                                let
-                                    numRepeats =
-                                        25 - List.length puzzlePageData.puzzles
-                                in
-                                puzzleListPage False Nothing { puzzlePageData | puzzles = puzzlePageData.puzzles ++ List.repeat numRepeats defaultPuzzleData } PuzzleListClickedPuzzle
-
-                            else
-                                puzzleListPage False Nothing puzzlePageData PuzzleListClickedPuzzle
+                            puzzleListPage puzzlePageData PuzzleListClickedPuzzle
 
                         Failure e ->
                             errorPage ""
@@ -69,118 +53,29 @@ view meta puzzleListState =
     ( title, body )
 
 
-defaultPuzzleData =
-    { id = 0
-    , themeTitle = "Lorem ipsum"
-    , puzzleSet = AbstractPuzzle
-    , imageLink = ""
-    , title = String.slice 0 20 loremIpsum
-    , isSolved = Nothing
-    }
-
-
-overlayDiv textString bgClass hasHover =
-    div [ class "relative w-full" ]
-        [ div [ class "absolute w-full pt-1", classList [ ( "group-hover:pt-0", hasHover ) ] ]
-            [ div [ class "flex justify-center align-center items-center w-full " ]
-                [ div [ class <| "px-3 py-2 text-bold text-white rounded-t-lg text-center w-full " ++ bgClass ] [ text textString ] ]
-            ]
-        ]
-
-
-puzzleCard : Bool -> (PuzzleId -> Msg) -> MiniPuzzleData -> Html Msg
-puzzleCard loadState onClickPuzzle puzzle =
+puzzleCard : MiniPuzzleData -> Html Msg
+puzzleCard puzzle =
     let
-        isLoading =
-            case loadState of
-                True ->
-                    True
-
-                False ->
-                    case puzzle.themeTitle of
-                        "Lorem ipsum" ->
-                            -- bit of a hack to differentiate non-open puzzles...
-                            True
-
-                        _ ->
-                            False
-
-        colour =
-            case isLoading of
-                True ->
-                    "grey"
-
-                _ ->
-                    puzzleColour puzzle.puzzleSet
-
-        onClickPuzzleAttrs =
-            case isLoading of
-                True ->
-                    []
-
-                False ->
-                    [ onClick (onClickPuzzle puzzle.id) ]
-
         overlay =
             case mapSolvedToBool puzzle.isSolved of
                 True ->
-                    overlayDiv "COMPLETED!" "bg-black" True
+                    div [ class "puzzle-overlay" ] []
 
                 False ->
-                    case ( isLoading, loadState ) of
-                        ( True, False ) ->
-                            overlayDiv "UNRELEASED" "bg-grey" False
-
-                        _ ->
-                            div [] []
-
-        opacityClass =
-            case mapSolvedToBool puzzle.isSolved of
-                True ->
-                    " opacity-25 "
-
-                False ->
-                    " opacity-100 "
-
-        bottomText =
-            case isLoading of
-                True ->
-                    ""
-
-                False ->
-                    "№ " ++ String.fromInt puzzle.id ++ " " ++ puzzle.title
+                    div [] []
     in
     div
-        [ class "block w-full  h-full w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/4 xl:w-1/5 p-1" ]
-        [ div
-            ([ class <| "group w-full rounded active:border-b-0 ", classList [ ( "cursor-pointer", not isLoading ) ] ] ++ onClickPuzzleAttrs)
-            [ overlay
-            , div
-                [ class "block w-full h-full rounded-b-lg border-b-4 pt-1 "
-                , classList
-                    [ ( "border-" ++ colour ++ "-dark", not isLoading )
-                    , ( "active:border-t-4 active:border-b-0 active:border-white group-hover:pt-0 group-hover:border-b-8", not isLoading )
-                    , ( "border-grey", isLoading )
-                    ]
-                ]
-                [ div
-                    [ class "flex h-full flex-col items-center align-center justify-center rounded-t-lg", classList [ ( "bg-" ++ colour, not isLoading ), ( "bg-grey-light", isLoading ) ] ]
-                    [ div [ class "w-full h-32 overflow-hidden rounded-t-lg" ]
-                        [ div [ class "w-full flex align-center items-center justify-center" ]
-                            [ img
-                                [ class "h-32", src puzzle.imageLink ]
-                                []
-                            ]
-                        ]
-                    , div
-                        [ class "w-full flex-grow items-center justify-center flex px-1 h-8 text-sm text-center"
-                        , classList [ ( "text-white bg-" ++ colour, not isLoading ), ( "bg-grey text-grey rounded", isLoading ) ]
-                        ]
-                        [ text bottomText ]
-                    ]
-                ]
-            ]
+        [ class "item"
+        , title <| String.fromInt puzzle.id ++ ". " ++ puzzle.title
+        , onClick <| PuzzleListClickedPuzzle puzzle.id
         ]
+        [ overlay
+        , img [ src puzzle.imageLink ] []
+        ]
+
+
+emptyPuzzle =
+    div [ class "item" ] []
 
 
 mapSolvedToBool : Maybe Bool -> Bool
@@ -198,65 +93,41 @@ mapSolvedToBool boolMaybe =
             False
 
 
-puzzleListPage : Bool -> Maybe String -> PuzzlePageData -> (PuzzleId -> Msg) -> Html Msg
-puzzleListPage isLoading errorMsg puzzlePageData onClickPuzzle =
+puzzleListPage : PuzzlePageData -> (PuzzleId -> Msg) -> Html Msg
+puzzleListPage puzzlePageData onClickPuzzle =
     let
         puzzles =
             puzzlePageData.puzzles
+
+        items =
+            List.map puzzleCard puzzles
+
+        extraItems =
+            if List.length puzzles < 25 then
+                List.repeat (25 - List.length puzzles) emptyPuzzle
+
+            else
+                []
+
+        next =
+            puzzlePageData.next
+
+        nextString =
+            [ b [] [ text next.theme ], text " releases ", text (Handlers.posixToString next.openDatetime), text " AEST (GMT+10)." ]
 
         pageTitle =
             "Puzzles"
 
         introText =
             div [] [ text "Click on a puzzle to view it." ]
-
-        errorDiv =
-            case errorMsg of
-                Just message ->
-                    div
-                        [ id "message-box", class "flex pin-b pin-x h-auto p-4 pb-6 fixed bg-grey-light text-grey-darkest justify-center text" ]
-                        [ span
-                            [ class "text-center md:w-3/4 " ]
-                            [ text "Hmm, it looks like there was an error. Let us know!" ]
-                        ]
-
-                Nothing ->
-                    div [] []
-
-        puzzlesDiv =
-            case List.length puzzles of
-                0 ->
-                    div [] [ text "There aren't any open puzzles yet...but stay tuned :)" ]
-
-                _ ->
-                    div [ class "block" ]
-                        [ div [ class "py-2" ]
-                            [ div [ class "pb-2" ] [ textWithLoad isLoading "Here's the colour coding for the puzzles:" ]
-                            , div [ class "py-1" ]
-                                [ puzzleSetSpan isLoading MetaPuzzle
-                                , textWithLoad isLoading " - The puzzle about all the puzzles!"
-                                ]
-                            , div [ class "py-1" ]
-                                [ puzzleSetSpan isLoading AbstractPuzzle
-                                , textWithLoad isLoading " - Puzzles which don't require any explicit coding, but do require a logical mindset and some medical knowledge."
-                                ]
-                            , div [ class "py-1" ]
-                                [ puzzleSetSpan isLoading BeginnerPuzzle
-                                , textWithLoad isLoading " - Puzzles for coding beginners with fill-in-the-blank Python tutorials included."
-                                ]
-                            , div [ class "py-1" ]
-                                [ puzzleSetSpan isLoading ChallengePuzzle
-                                , textWithLoad isLoading " - Puzzles for experienced coders and not the faint of heart!"
-                                ]
-                            ]
-                        , div [ class "flex flex-wrap" ] <| List.map (puzzleCard isLoading onClickPuzzle) puzzles
-                        ]
     in
-    pageBase
-        { iconSpan = span [ class "fas fa-book-reader" ] []
-        , isCentered = False
-        , colour = "yellow"
-        , titleSpan = text "Puzzles"
-        , bodyContent = puzzlesDiv
-        , outsideMain = div [] []
-        }
+    div
+        [ class "main" ]
+        [ div [ class "container" ]
+            [ div [ class "center" ]
+                [ div [ class "grid" ] (items ++ extraItems)
+                , div [ class "tagline" ]
+                    (br [] [] :: nextString)
+                ]
+            ]
+        ]
